721cc7621c30160cdc4744134bbc499e7ede0d1b
fiwosama <2561956090@qq.com>
2026-05-19 12:10:13 +0800

    perf: thread constant phi branches

--- src/ir/passes/SimplifyCFGPass.cpp
+++ src/ir/passes/SimplifyCFGPass.cpp
@@ -11,6 +11,7 @@
 #include "PhiInst.h"
 #include "ConstInt.h"
 #include "Function.h"
+#include "ICmpInst.h"
 #include "Instruction.h"
 #include "Module.h"
 #include "User.h"
@@ -81,6 +82,20 @@ bool phiHasIncomingFrom(PhiInst * phi, BasicBlock * block)
 	return false;
 }
 
+Value * phiValueFrom(PhiInst * phi, BasicBlock * block)
+{
+	if (phi == nullptr || block == nullptr) {
+		return nullptr;
+	}
+
+	for (const auto & incoming: phi->getIncomingValues()) {
+		if (incoming.second == block) {
+			return incoming.first;
+		}
+	}
+	return nullptr;
+}
+
 bool startsWithPhi(BasicBlock * block)
 {
 	return block != nullptr && !block->getInstructions().empty() &&
@@ -147,6 +162,187 @@ void replaceBranchTarget(BasicBlock * block, BasicBlock * oldTarget, BasicBlock 
 	branch->clearOperands();
 	branch->removeUses();
 	delete branch;
+}
+
+bool branchHasDoubleEdgeTo(BranchInst * branch, BasicBlock * target)
+{
+	return branch != nullptr && branch->isConditional() && branch->getTrueTarget() == target &&
+		   branch->getFalseTarget() == target;
+}
+
+bool evaluateICmp(ICmpInst::Predicate predicate, int32_t lhs, int32_t rhs)
+{
+	switch (predicate) {
+		case ICmpInst::Predicate::EQ:
+			return lhs == rhs;
+		case ICmpInst::Predicate::NE:
+			return lhs != rhs;
+		case ICmpInst::Predicate::SLT:
+			return lhs < rhs;
+		case ICmpInst::Predicate::SLE:
+			return lhs <= rhs;
+		case ICmpInst::Predicate::SGT:
+			return lhs > rhs;
+		case ICmpInst::Predicate::SGE:
+			return lhs >= rhs;
+	}
+	return false;
+}
+
+bool conditionForPredecessor(Value * condition, BasicBlock * pred, bool & takeTrueEdge)
+{
+	if (auto * phi = dynamic_cast<PhiInst *>(condition); phi != nullptr) {
+		auto * incoming = dynamic_cast<ConstInt *>(phiValueFrom(phi, pred));
+		if (incoming == nullptr) {
+			return false;
+		}
+		takeTrueEdge = incoming->getVal() != 0;
+		return true;
+	}
+
+	auto * icmp = dynamic_cast<ICmpInst *>(condition);
+	if (icmp == nullptr) {
+		return false;
+	}
+
+	auto * lhsPhi = dynamic_cast<PhiInst *>(icmp->getOperand(0));
+	auto * rhsPhi = dynamic_cast<PhiInst *>(icmp->getOperand(1));
+	auto * lhsConst = dynamic_cast<ConstInt *>(icmp->getOperand(0));
+	auto * rhsConst = dynamic_cast<ConstInt *>(icmp->getOperand(1));
+
+	if (lhsPhi != nullptr && rhsConst != nullptr) {
+		auto * incoming = dynamic_cast<ConstInt *>(phiValueFrom(lhsPhi, pred));
+		if (incoming == nullptr) {
+			return false;
+		}
+		takeTrueEdge = evaluateICmp(icmp->getPredicate(), incoming->getVal(), rhsConst->getVal());
+		return true;
+	}
+
+	if (lhsConst != nullptr && rhsPhi != nullptr) {
+		auto * incoming = dynamic_cast<ConstInt *>(phiValueFrom(rhsPhi, pred));
+		if (incoming == nullptr) {
+			return false;
+		}
+		takeTrueEdge = evaluateICmp(icmp->getPredicate(), lhsConst->getVal(), incoming->getVal());
+		return true;
+	}
+
+	return false;
+}
+
+bool isPhiBranchOnlyBlock(BasicBlock * block, Value * condition)
+{
+	if (block == nullptr || condition == nullptr) {
+		return false;
+	}
+
+	for (auto * inst: block->getInstructions()) {
+		if (dynamic_cast<PhiInst *>(inst) != nullptr) {
+			continue;
+		}
+		if (inst == condition) {
+			continue;
+		}
+		if (dynamic_cast<BranchInst *>(inst) != nullptr) {
+			continue;
+		}
+		return false;
+	}
+	return true;
+}
+
+bool blockValuesStayLocal(BasicBlock * block, const IRCFG & cfg)
+{
+	if (block == nullptr) {
+		return false;
+	}
+
+	for (auto * inst: block->getInstructions()) {
+		if (inst == nullptr) {
+			continue;
+		}
+		for (auto * use: inst->getUseList()) {
+			auto * userInst = dynamic_cast<Instruction *>(use->getUser());
+			if (userInst == nullptr) {
+				return false;
+			}
+			auto userBlock = cfg.instructionBlock.find(userInst);
+			if (userBlock == cfg.instructionBlock.end() || userBlock->second != block) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+bool foldConstantPhiBranches(Function * function)
+{
+	if (function == nullptr || function->isBuiltin()) {
+		return false;
+	}
+
+	IRCFG cfg = IRCFGBuilder::build(function);
+	for (auto * block: function->getBasicBlocks()) {
+		if (block == nullptr || block == function->getEntryBlock()) {
+			continue;
+		}
+		if (cfg.reachable.find(block) == cfg.reachable.end() || !startsWithPhi(block)) {
+			continue;
+		}
+
+		auto * branch = dynamic_cast<BranchInst *>(block->getTerminator());
+		if (branch == nullptr || !branch->isConditional() || branch->getOperandsNum() != 1) {
+			continue;
+		}
+		if (startsWithPhi(branch->getTrueTarget()) || startsWithPhi(branch->getFalseTarget())) {
+			continue;
+		}
+
+		auto * condition = branch->getOperand(0);
+		if (!isPhiBranchOnlyBlock(block, condition)) {
+			continue;
+		}
+		if (!blockValuesStayLocal(block, cfg)) {
+			continue;
+		}
+
+		auto predIter = cfg.predecessors.find(block);
+		if (predIter == cfg.predecessors.end() || predIter->second.empty()) {
+			continue;
+		}
+
+		std::vector<std::pair<BasicBlock *, BasicBlock *>> redirects;
+		bool canFold = true;
+		for (auto * pred: predIter->second) {
+			if (cfg.reachable.find(pred) == cfg.reachable.end()) {
+				continue;
+			}
+			auto * predBranch = dynamic_cast<BranchInst *>(pred->getTerminator());
+			if (branchHasDoubleEdgeTo(predBranch, block)) {
+				canFold = false;
+				break;
+			}
+
+			bool takeTrueEdge = false;
+			if (!conditionForPredecessor(condition, pred, takeTrueEdge)) {
+				canFold = false;
+				break;
+			}
+			redirects.emplace_back(pred, takeTrueEdge ? branch->getTrueTarget() : branch->getFalseTarget());
+		}
+
+		if (!canFold || redirects.empty()) {
+			continue;
+		}
+
+		for (const auto & redirect: redirects) {
+			replaceBranchTarget(redirect.first, block, redirect.second);
+		}
+		return true;
+	}
+
+	return false;
 }
 
 void collectInstructions(const BlockSet & blocks, InstSet & instructions)
@@ -343,6 +539,7 @@ bool SimplifyCFGPass::run(Module & module)
 		while (changed) {
 			changed = false;
 			changed |= foldConstantBranches(function);
+			changed |= foldConstantPhiBranches(function);
 			changed |= removeUnreachableBlocks(function);
 			changed |= bypassEmptyJumpBlock(function);
 		}
